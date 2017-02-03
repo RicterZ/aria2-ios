65819e890ffdf4e6f1fe51540ab933a635561b82
Ricter Z <ricterzheng@gmail.com>
2017-02-04 00:14:40 +0800

    Initial Commit

---
+++ aria2-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  aria2-ios
+//
+//  Created by Ricter Zheng on 2017/2/4.
+//  Copyright © 2017年 Ricter Zheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
